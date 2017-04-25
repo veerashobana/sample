d66c65653dce4ba5107a87d9c04360f8a0d53e25
veerashobana <veera.balasundaram@wipro.com>
2017-04-25 10:59:31 +0530

    removed conio functionalities

--- test.c
+++ test.c
@@ -4,13 +4,11 @@ Author: Vinoth Selvaraj
 */
 //Header files
 #include <stdio.h>
-#include <conio.h>
  
 void main()
 {
     //Program variables
     int cn,rge,num1=0,num2=1,num3;
-    clrscr(); //Function to clear previous output
     //Display function
     printf("Enter the range of Fibonacci series to generate : ");
     scanf("%d",&rge); //Getting input function
@@ -25,5 +23,4 @@ void main()
         num1=num2;
         num2=num3;
     }
-    getch();
 }
