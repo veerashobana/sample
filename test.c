2c0bcf1046ed972e4fc95406a0e9e6a17d86543b
veerashobana <veera.balasundaram@wipro.com>
2017-04-25 12:51:29 +0530

    webhook test-added spaces

--- test.c
+++ test.c
@@ -8,19 +8,19 @@ Author: Vinoth Selvaraj
 void main()
 {
     //Program variables
-    int cn,rge,num1=0,num2=1,num3;
+    int cn, rge, num1=0, num2=1, num3;
     //Display function
     printf("Enter the range of Fibonacci series to generate : ");
-    scanf("%d",&rge); //Getting input function
+    scanf("%d", &rge); //Getting input function
  
-    printf("%d-->%d-->",num1,num2);
+    printf("%d-->%d-->", num1, num2);
  
     //Creating fibonacci series.
-    for(cn=2;cn<rge;cn++) //Looping statement
+    for(cn = 2; cn < rge; cn++) //Looping statement
     {
-        num3=num1+num2;
-        printf("%d-->",num3);
-        num1=num2;
-        num2=num3;
+        num3 = num1 + num2;
+        printf("%d-->", num3);
+        num1 = num2;
+        num2 = num3;
     }
 }
